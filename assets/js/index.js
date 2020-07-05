5d1cc0d4eee09e6cd8b00c34ec2ead26af2c24ee
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-07-05 23:46:59 +0530

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+
+let menuButton=document.querySelector("#menu");
+let menuItems=document.querySelector("#menu-items");
+menuButton.addEventListener('click',function(){
+    if(menuItems.classList.contains('menu-close')){
+        menuItems.classList.remove('menu-close');
+    }
+    else{
+        menuItems.classList.add('menu-close');
+    }
+})
