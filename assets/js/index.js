6405718bbde9caa61fea2e4989c5f6ece0a9ca89
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-10-01 23:25:23 +0530

    updated version

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,22 @@
+  
+  const config={
+    type: 'carousel',
+  perView: 4,
+  focusAt: 'center',
+  breakpoints: {
+  
+    800: {
+      perView: 2
+    },
+    480: {
+      perView: 1
+    }}
+  };
+
+
+    new Glide('.glide', config).mount()
+
+
 
 let menuButton=document.querySelector("#menu");
 let menuItems=document.querySelector("#menu-items");
@@ -9,3 +28,40 @@ menuButton.addEventListener('click',function(){
         menuItems.classList.add('menu-close');
     }
 });
+
+
+/*menu navigation*/
+let array=['home', 'skills', 'projects', 'contact']
+$.each($('.nav-item'), function(key, value){
+  $(value).on('click', function(){
+    let sec= $(value).children('a').attr('href').slice(1,)
+  
+    $.each(array, function(k, v){
+      $(`#${v}`).addClass('hide');
+    })
+    $(`#${sec}`).removeClass('hide')
+  
+  }) 
+})
+
+function setToggle(){
+  document.body.scrollLeft=0;
+  
+  if(window.screen.width>=768){
+    $('.projects-container').removeClass('hide')
+    $('.projects-container-2').addClass('hide')
+  }
+  else{
+    $('.projects-container-2').removeClass('hide')
+    $('.projects-container').addClass('hide')
+  }
+
+}
+window.addEventListener("load", setToggle)
+window.addEventListener("resize", setToggle);
+
+$.each($('.p-img>img'),function(k,v){
+  let val=$(v).attr('alt')
+  $(v).parent().children('.img-overlay').children('h4').html(val)
+})
+
